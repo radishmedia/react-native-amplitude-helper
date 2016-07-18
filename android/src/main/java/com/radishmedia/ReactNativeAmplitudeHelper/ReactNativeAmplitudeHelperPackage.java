e75fb21a2c73be51faa9302b7a8fa4edb6d1af54
Joy Cho <joyeon@gmail.com>
2016-07-18 22:59:54 +0900

    Add java utils

--- android/src/main/java/com/radishmedia/ReactNativeAmplitudeHelper/ReactNativeAmplitudeHelperPackage.java
+++ android/src/main/java/com/radishmedia/ReactNativeAmplitudeHelper/ReactNativeAmplitudeHelperPackage.java
@@ -9,9 +9,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ReactNativeAmplitudeHelperPackage implements ReactPackage {
 
